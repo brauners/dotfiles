ee0942a7e5c6f3fd8ed1e6d1ef8f14a3781a0ad2
Andre Braunersreuther <andre.braunersreuther@smarteye.se>
2023-12-12 19:29:03 +0100

    Add install script packages

---
+++ .chezmoiscripts/run_onchange_before_install_packages.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+sudo apt install curl -y
+sudo apt install git -y
+sudo apt install git-gui -y
+sudo apt install fish -y
+sudo apt install tmux -y
+sudo apt install firefox -y
+sudo apt install thefuck -y
